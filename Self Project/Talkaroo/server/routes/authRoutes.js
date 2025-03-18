3d16a02bb2cdcf24fb3d07341291f007b7287bae
josh-delfin <123255126+josh-delfin@users.noreply.github.com>
2025-03-18 10:34:10 +0800

    Fix PostgreSQL connection issue

--- Self Project/Talkaroo/server/routes/authRoutes.js
+++ Self Project/Talkaroo/server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registerUser, loginUser, loginAdmin } = require("../controllers/authController");
+const { registerUser, loginUser, loginAdmin } = require("../Controllers/authController");
 
 const router = express.Router();
 
